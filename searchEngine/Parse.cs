77ea2a82d0b23414aeec91b4499d66bf17aaa929
amitp <amitp@LAPTOP-S98NLD06>
2016-12-11 21:53:57 +0200

    Add project files.

---
+++ searchEngine/Parse.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace searchEngine
+{
+    public class Parse
+    {
+        Dictionary<string, string> dates;
+
+        public Parse()
+        {
+            dates = new Dictionary<string, string>()
+            {
+                {"JANUARY","01" },
+                 {"JAN","01" },
+                 {"FEBUARY","02" },
+                {"FEB","02" },
+                {"MARCH","03" },
+                {"APRIL","04" },
+                {"APR","04" },
+                {"MAY","05" },
+                {"JUNE","06" },
+                {"JUN","06" },
+                {"JULY","07" },
+                {"JUL","07" },
+                {"AUGUST","08" },
+                {"AUG","08" },
+                {"SEPTEMBER","09" },
+                {"SEP","09" },
+                {"OCTOBER","10" },
+                {"OCT","10" },
+                {"NOVEMBER","11" },
+                {"NOV","11" },
+                {"DECEMBER","12" },
+                {"DEC","12" },
+            };
+
+            }
+        public Dictionary<string,int> parseDocument(string doc)
+        {
+           bool shouldContinue = false;
+            char[] delimiters = { ' ', '\n', ';', ':', '"', '(', ')', '[', ']', '{', '}', '*' };
+           string[] initialArrayOfDoc= doc.Trim(delimiters).Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> terms = new Dictionary<string, int>();
+            for(int i = 0; i < initialArrayOfDoc.Length; i++)
+            {
+                string currentTerm = initialArrayOfDoc[i];
+                //doesnt contain a digit then
+                if (!currentTerm.Any(char.IsDigit))
+                {
+                    if (currentTerm.ToUpper() == "BETWEEN")
+                    {
+
+                    }
+                    else
+                    {
+                        ///now check if term is part of a date
+                        if (dates.ContainsKey(currentTerm.ToUpper())&&i<initialArrayOfDoc.Length-1)
+                        {
+                            string nextTerm = initialArrayOfDoc[i + 1];
+                            bool hasComma = false;
+                            if (nextTerm[nextTerm.Length - 1] == ',')
+                            {
+                                nextTerm = nextTerm.TrimEnd(',');
+                                hasComma = true;
+                            }
+                                int valueOfDay;
+                                if (int.TryParse(nextTerm, out valueOfDay))
+                                {
+                                    if (valueOfDay > 0 && valueOfDay <= 31)
+                                    {
+                                        string month = currentTerm;
+                                        currentTerm = "";
+                                        string sValueOfDay = "";
+                                        sValueOfDay+=valueOfDay;
+                                        if (valueOfDay < 10)
+                                            sValueOfDay = "0" + sValueOfDay;
+                                        currentTerm += dates[month.ToUpper()] + "-" + sValueOfDay;
+                                        //the doc is not long enough
+                                        int valueOfyear;                                     
+                                        //check if MM-DD-YYYY
+                                        if (i < initialArrayOfDoc.Length - 2&&hasComma &&int.TryParse(initialArrayOfDoc[i + 2], out valueOfyear)&& valueOfyear>999&& valueOfyear<=9999)
+                                        {
+                                            currentTerm = valueOfyear + "-" + currentTerm;                                              
+                                            i = i + 2;
+                                        }
+                                        else
+                                        {
+                                        //insert MM-DD
+                                            insertToDic(terms, currentTerm);
+                                            i = i + 1;
+                                            continue;
+                                        }
+                                    }
+                                    //check if mm-yyyy
+                                    else
+                                    {
+                                        int valueOfYear = valueOfDay;
+                                        if(valueOfDay > 999 && valueOfDay <= 9999)
+                                        {
+                                            string month = currentTerm;
+                                            currentTerm = "";
+                                            currentTerm = valueOfYear + "-" + dates[month.ToUpper()];
+                                        insertToDic(terms, currentTerm);
+                                         i = i + 1;
+                                         continue;
+                                        }
+                                    }
+                                }
+                            }
+                    }
+                }
+
+                else
+                {
+                    shouldContinue = startWithNumber(terms, initialArrayOfDoc, currentTerm,ref i);
+                    if (shouldContinue)
+                        continue;
+                }
+                if(currentTerm!="")
+                    insertToDic(terms, currentTerm.Trim(',','.','-'));
+            }
+            return terms;
+        }
+        //check what to do 
+        private bool startWithNumber(Dictionary<string, int> terms, string[] initialArrayOfDoc, string currentTerm,ref int i)
+        {
+            bool shouldContinue = false;
+            if (currentTerm.Length > 2)
+            {
+                int valueOfDay;
+                //check if ends with th
+                if (i<initialArrayOfDoc.Length-1&&currentTerm.Substring(currentTerm.Length - 2) == "th"&& int.TryParse(currentTerm.Substring(0,currentTerm.Length - 2), out valueOfDay)&& valueOfDay>=1&& valueOfDay<=31)
+                {
+                    string day = valueOfDay<10 ? "0" + valueOfDay.ToString() : valueOfDay.ToString();
+                    shouldContinue=checkIfNextIsDate(terms, day, initialArrayOfDoc, ref i);
+                    if (shouldContinue)
+                        return true;
+                }
+            }
+            else
+            {
+                int value;
+                //check if number
+                if(int.TryParse(currentTerm, out value))
+                    {
+                    if(value >= 1 && value <= 31)
+                    {
+                        shouldContinue = checkIfNextIsDate(terms, value.ToString(), initialArrayOfDoc, ref i);
+                        if (shouldContinue)
+                            return true;
+                    }
+                    //real number
+
+
+
+                    }
+            }
+            return false;
+        }
+        private bool checkIfNextIsDate(Dictionary<string, int> terms, string day, string[] initialArrayOfDoc, ref int i)
+        {
+            if (dates.ContainsKey(initialArrayOfDoc[i + 1].ToUpper())){
+                if(i+1 < initialArrayOfDoc.Length - 1)
+                {
+                    int valueOfYear;
+                    //check if the next next value is a number
+                    if(int.TryParse(initialArrayOfDoc[i+2],out valueOfYear))
+                    {
+                        //check if DD-mm-yy
+                        if (valueOfYear >= 0 && valueOfYear <= 99)
+                        {
+                            string yearPrefix;
+                            yearPrefix = (valueOfYear >= 0 && valueOfYear <= 20) ? "20" :  "19";
+                            string termToAdd = yearPrefix + "" + valueOfYear + "-" + dates[initialArrayOfDoc[i + 1].ToUpper()] + "-" + day;
+                            insertToDic(terms, termToAdd);
+                            i = i + 2;
+                            return true;                    
+                        }
+                        else
+                        {
+                            //check if DD-mm-YYYY
+                            if (valueOfYear>=1000&&valueOfYear<=9999)
+                            {
+                                string termToAdd = valueOfYear + "-" + dates[initialArrayOfDoc[i + 1].ToUpper()] + "-" + day;
+                                insertToDic(terms, termToAdd);
+                                i = i + 2;
+                                return true;
+                            }
+                        }
+                    }
+                    else
+                         //case of DD month
+                     {
+                        string termToAdd =dates[initialArrayOfDoc[i + 1].ToUpper()] + "-" + day;
+                        insertToDic(terms, termToAdd);
+                        i = i + 1;
+                         return true;
+                      }
+                }
+              
+            }
+            return false;
+        }
+
+        private void insertToDic(Dictionary<string,int> terms ,string term)
+        {
+            if (terms.ContainsKey(term))
+                terms[term]++;
+            else
+                terms.Add(term, 1);
+        }
+        }
+    }
+
---
+++ testEngine/UnitTest1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using searchEngine;
+using System.Collections.Generic;
+
+namespace testEngine
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Parse parse = new Parse();
+        Dictionary<string, int> terms;
+        Dictionary<String, int> expectedTerms = new Dictionary<string, int>();
+        string doc;
+
+        [TestMethod]
+        public void TestOnlyAdam()
+        {
+            doc = "adam*";
+            terms = parse.parseDocument(doc);
+            expectedTerms.Add("adam", 1);
+            Assert.AreEqual(true, checkEqulas());
+        }
+
+        [TestMethod]
+        public void testMonthDD()
+        {
+            doc = "June 4";
+            terms = parse.parseDocument(doc);
+            expectedTerms.Add("06-04", 1);
+            Assert.AreEqual(true, checkEqulas());
+        }
+
+        [TestMethod]
+        public void testMonthDDWithAdam()
+        {
+            doc = "June 4 adam July 5";
+            terms = parse.parseDocument(doc);
+            expectedTerms.Add("06-04", 1);
+            expectedTerms.Add("adam", 1);
+            expectedTerms.Add("07-05", 1);
+            Assert.AreEqual(true, checkEqulas());
+        }
+
+        [TestMethod]
+        public void testMonthDDYYYY()
+        {
+            doc = "April 28, 1990";
+            terms = parse.parseDocument(doc);
+            expectedTerms.Add("1990-04-28", 1);
+            Assert.AreEqual(true, checkEqulas());
+        }
+
+        [TestMethod]
+        public void testMonthYYYY()
+        {
+            doc = "May 1994";
+            terms = parse.parseDocument(doc);
+            expectedTerms.Add("1994-05", 1);
+            Assert.AreEqual(true, checkEqulas());
+        }
+
+        [TestMethod]
+        public void testMonthMixed()
+        {
+            doc = "In may 1994 adam was nash-nash. May was pretty. jan 28, 2005 july 7 june 8000 october fest";
+            terms = parse.parseDocument(doc);
+            insertToExpected("In;1994-05;adam;was;nash-nash;May;was;pretty;2005-01-28;07-07;8000-06;october;fest");
+            Assert.AreEqual(true, checkEqulas());
+        }
+        [TestMethod]
+        public void testMonthMixed2()
+        {
+            doc = "may 5  may 5";
+            terms = parse.parseDocument(doc);
+            insertToExpected("05-05;05-05");
+            Assert.AreEqual(true, checkEqulas());
+        }
+
+        [TestMethod]
+        public void testNumberFirstDate()
+        {
+            doc = "16th march 1990";
+            terms = parse.parseDocument(doc);
+            insertToExpected("1990-03-16");
+            Assert.AreEqual(true, checkEqulas());
+        }
+
+        [TestMethod]
+        public void testNumberFirstDate2()
+        {
+            doc = "16th march 91";
+            terms = parse.parseDocument(doc);
+            insertToExpected("1991-03-16");
+            Assert.AreEqual(true, checkEqulas());
+        }
+
+        [TestMethod]
+        public void testNumberFirstDate3()
+        {
+            doc = "adam 10th feb 15 amit";
+            terms = parse.parseDocument(doc);
+            insertToExpected("adam;2015-02-10;amit");
+            Assert.AreEqual(true, checkEqulas());
+        }
+        [TestMethod]
+        public void testNumberFirstDate4()
+        {
+            doc = "adam 14 may 1991 amit";
+            terms = parse.parseDocument(doc);
+            insertToExpected("adam;1991-05-14;amit");
+            Assert.AreEqual(true, checkEqulas());
+        }
+        [TestMethod]
+        public void testNumberFirstDate5()
+        {
+            doc = "adam 14 may amit yonatan. 78 aug * * jan 50";
+            terms = parse.parseDocument(doc);
+            insertToExpected("adam;05-14;amit;yonatan;78;aug;jan;50");
+            Assert.AreEqual(true, checkEqulas());
+        }
+        private void insertToExpected(string doc)
+        {
+            string[] docArray = doc.Split(';');
+            foreach(string s in docArray)
+            {
+                if(expectedTerms.ContainsKey(s))
+                    expectedTerms[s]++;
+                else
+                {
+                    expectedTerms.Add(s, 1);
+                }
+            }
+        }
+
+        private bool checkEqulas()
+        {
+            bool equal = false;
+            if (expectedTerms.Count == terms.Count) // Require equal count.
+            {
+                equal = true;
+                foreach (var pair in terms)
+                {
+                    int value;
+                    if (expectedTerms.TryGetValue(pair.Key, out value))
+                    {
+                        // Require value be equal.
+                        if (value != pair.Value)
+                        {
+                            equal = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        // Require key be present.
+                        equal = false;
+                        break;
+                    }
+                }
+            }
+            return equal;
+        }
+    }
+}
