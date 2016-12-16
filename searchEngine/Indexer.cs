8cb1b2bef5b8d418ec5f9500f086a5c1a8f2aba3
Adam Zamri <zamri@post.bgu.ac.il>
2016-12-16 14:35:00 +0200

    new class indexer

---
+++ searchEngine/Indexer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace searchEngine
+{
+    class Indexer
+    {
+        Dictionary<string,int[,]> m_termsDictionary;
+        //get a list of parser result for a few document
+        //the string represents the 
+        public void indexBatch(List<Dictionary<string,Term>> documentsAfterParse)
+        {
+
+
+
+
+
+
+        }
+
+    }
+}
