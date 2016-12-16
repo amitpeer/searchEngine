58c2b185130be2f4af563420d037dea3be07f3e0
Adam Zamri <zamri@post.bgu.ac.il>
2016-12-16 18:00:37 +0200

    manage searcher and indexer

--- searchEngine/Indexer.cs
+++ searchEngine/Indexer.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,21 +10,27 @@ namespace searchEngine
 {
     public class Indexer
     {
-        Dictionary<string,int[]> m_termsDictionary;
+        Dictionary<string, int[]> m_termsDictionary;
+        private string path;
+        private int counterFiles;
+
         //get a list of parser result for a few document
         //the string represents the 
-        public Indexer(){
+        public Indexer(string pathToSave)
+        {
             m_termsDictionary = new Dictionary<string, int[]>();
+            path = pathToSave;
 
         }
-        public void indexBatch(List<Dictionary<string,TermInfoInDoc>> documentsAfterParse)
+        public void indexBatch(List<Dictionary<string, TermInfoInDoc>> documentsAfterParse)
         {
-            Dictionary<string, Term> miniPostingFIle = new Dictionary<string, Term>();
-            foreach(Dictionary<string,TermInfoInDoc> parserResult in documentsAfterParse)
+            Dictionary<string, Term> miniPostingFile = new Dictionary<string, Term>();
+            foreach (Dictionary<string, TermInfoInDoc> parserResult in documentsAfterParse)
             {
-               List<string> terms=parserResult.Keys.ToList();
-                foreach(string term in terms)
+                List<string> terms = parserResult.Keys.ToList();
+                foreach (string term in terms)
                 {
+                    /*
                     if (m_termsDictionary.ContainsKey(term))
                     {
                         m_termsDictionary[term][0] = (m_termsDictionary[term])[0]++;
@@ -31,31 +39,33 @@ namespace searchEngine
                     {
                         m_termsDictionary.Add(term,new int[] {1,0});
                     }
-                    if (miniPostingFIle.ContainsKey(term))
+                    if (miniPostingFile.ContainsKey(term))
                     {
-                        miniPostingFIle[term].TermInDocument.Add(parserResult[term].)
-
-
+                        miniPostingFile[term].TermInDocument.Add(parserResult[term].DocName, parserResult[term]);
                     }
-
-
-
+                    else
+                    {
+                        Dictionary<string,TermInfoInDoc> m_term = new Dictionary<string, TermInfoInDoc>();
+                        m_term.Add(parserResult[term].DocName, parserResult[term]);
+                        Term termToInsert = new Term(m_term);
+                        miniPostingFile.Add(term, termToInsert);
+                    }
+                 }*/
                 }
+                List<string> sortedTerms = miniPostingFile.Keys.ToList<string>();
+                sortedTerms.Sort();
+                foreach (string s in sortedTerms)
+                {
+                    KeyValuePair<string, Term> toInsert = new KeyValuePair<string, Term>(s, miniPostingFile[s]);
 
-
-
-
-
-
-
-
+                    string json = JsonConvert.SerializeObject(toInsert, Formatting.Indented);
+                    if (File.Exists(path + "miniPosting" + counterFiles + ".json")){
+                        File.AppendAllText(path + "miniPosting" + counterFiles + ".json", json);
+                        }
+                    else
+                        System.IO.File.WriteAllText(path + "miniPosting" + counterFiles + ".json", json);
+                }
             }
-
-
-
-
-
         }
-
     }
 }
---
+++ searchEngine/ManageSearch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace searchEngine
+{
+    class ManageSearch
+    {
+        public void main()
+        {
+            ReadFile readFile = new ReadFile("C:\\Users\adamz\\Documents\\Visual Studio 2015\\Projects\folder");
+
+
+
+
+
+        }
+    }
+}
