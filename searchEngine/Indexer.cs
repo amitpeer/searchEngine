e363e65b09ebf831873b2468e28f73db4084a025
Adam Zamri <zamri@post.bgu.ac.il>
2016-12-18 21:03:05 +0200

    arranged func mergefiles

--- searchEngine/Indexer.cs
+++ searchEngine/Indexer.cs
@@ -68,33 +68,40 @@ namespace searchEngine
             int numOfFiles = Directory.GetFiles(m_pathToSave).Length;
             SortedDictionary<string,TermWithReader> termsInComparisonForMerge = new SortedDictionary<string, TermWithReader>();
             Dictionary<string, BinaryReader> BinaryReaders = new Dictionary<string, BinaryReader>();
-            foreach(string file in Directory.GetFiles(m_pathToSave))
+            //open a binary reader for each file
+            foreach (string file in Directory.GetFiles(m_pathToSave))
             {
                 BinaryReader br = new BinaryReader(File.Open(file, FileMode.Open));
-                BinaryReaders.Add(file,br);
-                checkAgain:
-                if (BinaryReaders[file].BaseStream.Position != BinaryReaders[file].BaseStream.Length)
+                BinaryReaders.Add(file, br);
+            }
+            //insert the first value for each reader
+            foreach(KeyValuePair<string,BinaryReader> br in BinaryReaders)
+            {
+                BinaryReader b = br.Value;
+                string pathOfFileRead = br.Key;
+                tryagain:
+                if (b.BaseStream.Position != b.BaseStream.Length)
                 {
-                    //get next term from BinaryReader
-                    string line = BinaryReaders[file].ReadString();
+                    string line = b.ReadString();
                     Term currentTerm = JsonConvert.DeserializeObject<Term>(line);
                     if (termsInComparisonForMerge.ContainsKey(currentTerm.M_termName))
                     {
-                        termsInComparisonForMerge[currentTerm.M_termName].Term.M_tid.Concat(currentTerm.M_tid).ToDictionary(x => x.Key, x => x.Value);
-                        goto checkAgain;
+                        termsInComparisonForMerge[currentTerm.M_termName].Term.M_tid = termsInComparisonForMerge[currentTerm.M_termName].Term.M_tid.Concat(currentTerm.M_tid).ToDictionary(x => x.Key, x => x.Value);
+                        goto tryagain;
                     }
                     else
                     {
-                        termsInComparisonForMerge.Add(currentTerm.M_termName, new TermWithReader(currentTerm, file));
+                        termsInComparisonForMerge.Add(currentTerm.M_termName, new TermWithReader(currentTerm, pathOfFileRead));
                     }
                 }
                 else
                 {
-                    BinaryReaders[file].Close();
-                    BinaryReaders.Remove(file);
-                    File.Delete(file);
+                    b.Close();
+                    BinaryReaders.Remove(pathOfFileRead);
+                    File.Delete(pathOfFileRead);
                 }
             }
+
             BinaryWriter writer = new BinaryWriter(File.Open(m_pathToSave + "\\MainPosting.bin", FileMode.Append));
             while (BinaryReaders.Count > 0)
             {
@@ -111,7 +118,7 @@ namespace searchEngine
                     Term currentTerm = JsonConvert.DeserializeObject<Term>(line);
                     if (termsInComparisonForMerge.ContainsKey(currentTerm.M_termName))
                     {
-                        termsInComparisonForMerge[currentTerm.M_termName].Term.M_tid.Concat(currentTerm.M_tid).ToDictionary(x => x.Key, x => x.Value);
+                        termsInComparisonForMerge[currentTerm.M_termName].Term.M_tid=termsInComparisonForMerge[currentTerm.M_termName].Term.M_tid.Concat(currentTerm.M_tid).ToDictionary(x => x.Key, x => x.Value);
                         goto nextTerminBinaryReader;
                     }
                     else
