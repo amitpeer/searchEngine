ae8718417c2ee997c659358a359487c2367e81b7
Adam Zamri <zamri@post.bgu.ac.il>
2016-12-19 11:48:50 +0200

    Its working!

--- searchEngine/ManageSearch.cs
+++ searchEngine/ManageSearch.cs
@@ -16,8 +16,8 @@ namespace searchEngine
         {
             Stopwatch m_stopwatch = new Stopwatch();
             m_stopwatch.Start();
-            string path = "C:\\Users\\amitp\\Documents\\לימודים\\סמסטר ה\\אחזור\\מנוע\\corpus\\corpus";
-            string pathToSave = "C:\\Users\\amitp\\Documents\\לימודים\\סמסטר ה\\אחזור\\מנוע\\corpus\\results";
+            string path = "C:\\Users\\adamz\\Documents\\Visual Studio 2015\\Projects\\folder\\test1";
+            string pathToSave = "C:\\Users\\adamz\\Documents\\Visual Studio 2015\\Projects\\folder\\results";
             bool shouldStem = false;
             ReadFile readFile = new ReadFile(path);
             readFile.ExtractStopWordsFile();
@@ -26,6 +26,7 @@ namespace searchEngine
             int numOfFiles=Directory.GetFiles(path).Length-1;
             int j = 11;
             //create miniPostingFile
+            /*
             for (int i = 1; i <= numOfFiles; i=i+10)
             {
                 List<string> batchOfDocs = readFile.getFiles(i, j);
@@ -37,7 +38,18 @@ namespace searchEngine
                 indexer.indexBatch(documentsAfterParse);   
                 j = j +10;
             }
-            indexer.MergeFiles();
+            */
+            for (int i = 1; i <= numOfFiles; i++)
+            {
+                List<string> batchOfDocs = readFile.getFile(i);
+                List<Dictionary<string, TermInfoInDoc>> documentsAfterParse = new List<Dictionary<string, TermInfoInDoc>>();
+                foreach (string s in batchOfDocs)
+                {
+                    documentsAfterParse.Add(parser.parseDocument(s, shouldStem));
+                }
+                indexer.indexBatch(documentsAfterParse);
+            }
+                indexer.MergeFiles();
 
 
         }
